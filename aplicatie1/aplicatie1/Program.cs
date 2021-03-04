61855751429570c3e0df4142a7941b3d7c65830f
ManoleGabrielaLoredana <gabriela.loredana99@yahoo.com>
2021-03-04 23:53:09 +0200

    Added solution for ex 2.

---
+++ aplicatie1/aplicatie1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace aplicatie1
+{
+    public abstract class Account
+    {
+        public decimal Balance { get; private set; }
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            var fee = CalculateWithDrawFee(amount);
+            amount += fee;
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Insufficient funds!");
+            }
+            this.Balance -= amount;
+            return amount;
+        }
+        protected abstract decimal CalculateWithDrawFee(decimal amount);
+    }
+    public class DebitAccount : Account
+    {
+        protected override decimal CalculateWithDrawFee(decimal amount)
+        {
+            return 0m;
+        }
+    }
+    public class SavingAccount : Account
+    {
+        protected override decimal CalculateWithDrawFee(decimal amount)
+        {
+            return amount * 0.5m / 100m;
+        }
+    }
+    class Program
+    {
+        private const int Amount = 500;
+        static void Main(string[] args)
+        {
+            var account = new SavingAccount();
+            account.Deposit(200);
+            System.Console.WriteLine($"Disponibil: {account.Balance} RON");
+            try
+            {
+                account.Withdraw(100);
+            }
+            catch(InvalidOperationException)
+            {
+                System.Console.WriteLine("Fonduri insuficiente");
+            }
+            System.Console.WriteLine($"Disponibil: {account.Balance} RON");
+        }
+    }
+}
